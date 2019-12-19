f94dbf689bb983dca17e68699ea6f36b12911ed3
rkfqlwjstk <julong01@naver.com>
2019-12-19 20:30:13 +0900

    18 chapter upload

--- apollo.js
+++ apollo.js
@@ -1,5 +1,5 @@
 const options = {
-    uri: "http://c34e5501.ngrok.io"
+    uri: "https://e57670af.ngrok.io"
   };
   
 export default options;
--- navigation/PhotoNavigation.js
+++ navigation/PhotoNavigation.js
@@ -4,27 +4,64 @@ import SelectPhoto from "../screens/Photo/SelectPhoto";
 import TakePhoto from "../screens/Photo/TakePhoto";
 import UploadPhoto from "../screens/Photo/UploadPhoto";
 import { stackStyles } from "./config";
+import styles from "../styles";
 
 const PhotoTabs = createMaterialTopTabNavigator(
     {
-      SelectPhoto,
-      TakePhoto
+      Select: {
+        screen: SelectPhoto,
+        navigationOptions: {
+          tabBarLabel: "Select"
+        }
+      },
+      Take: {
+        screen: TakePhoto,
+        navigationOptions: {
+          tabBarLabel: "Take"
+        }
+      }
     },
     {
-      tabBarPosition: "bottom"
+      tabBarPosition: "bottom",
+      tabBarOptions: {
+        indicatorStyle: {
+          backgroundColor: styles.blackColor,
+          marginBottom: 20
+        },
+        labelStyle: {
+          color: styles.blackColor,
+          fontWeight: "600"
+        },
+        style: {
+          paddingBottom: 20,
+          ...stackStyles
+        }
+      }
     }
 );
   
 export default createStackNavigator(
   {
-    PhotoTabs,
-    UploadPhoto
+    Tabs: {
+      screen: PhotoTabs,
+      navigationOptions: {
+        title: "Choose Photo",
+        headerBackTitle: null
+      }
+    },
+    Upload: {
+      screen: UploadPhoto,
+      navigationOptions: {
+        title: "Upload"
+      }
+    }
   },
   {
     defaultNavigationOptions: {
       headerStyle: {
         ...stackStyles
-      }
+      },
+      headerTintColor: styles.blackColor
     }
   }
 );
